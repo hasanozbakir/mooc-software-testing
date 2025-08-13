fe4a277e25e12bc1f072014090c27300888bc639
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-08-13 12:16:42 +0300

    NumFinder class bug is fixed

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -5,10 +5,8 @@ public class NumFinderMain {
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
         // nf.find(new int[] {4, 25, 7, 9});
 
-        // this crashes
         nf.find(new int[] {4, 3, 2, 1});
 
         System.out.println(nf.getLargest());
